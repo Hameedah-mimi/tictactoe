bc253e345d49b1071da7a79e09df841999143ed2
Hameedah-mimi <hameedahadeyemi1@gmail.com>
2026-07-12 08:56:13 +0100

    Remove unnecessary blank line in onContainerTap method

--- tictactoe/lib/grid.dart
+++ tictactoe/lib/grid.dart
@@ -18,7 +18,6 @@ class _GridPageState extends State<GridPage> {
       } else if (!xPlayerTurn && board[index] == "") {
         board[index] = "O";
       }
-
       xPlayerTurn = !xPlayerTurn;
     });
   }
